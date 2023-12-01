52230d0700bd5cf97255888c03e1f9600c05388b
Zeitsperre <10819524+Zeitsperre@users.noreply.github.com>
2023-12-01 11:09:33 -0500

    various workflow improvements

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -8,22 +8,22 @@ def remove_file(filepath):
     Path(PROJECT_DIRECTORY).joinpath(filepath).unlink()
 
 
-def remove_folder(folderpath):
-    for file in Path(PROJECT_DIRECTORY).joinpath(folderpath).iterdir():
+def remove_folder(folder_path):
+    for file in Path(PROJECT_DIRECTORY).joinpath(folder_path).iterdir():
         if not file.is_dir():
             file.unlink()
             continue
         file.rmdir()
-    Path(PROJECT_DIRECTORY).joinpath(folderpath).rmdir()
+    Path(PROJECT_DIRECTORY).joinpath(folder_path).rmdir()
 
 
 def replace_contents(filepath):
     replacements = {
-        "__GITHUB_REF__": "github.ref",
+        "__BUMPVERSION_TOKEN__": "secrets.BUMPVERSION_TOKEN",
+        "__GITHUB_REF_NAME__": "github.ref_name",
         "__GITHUB_TOKEN__": "secrets.GITHUB_TOKEN",
         "__PYTHON_VERSION__": "matrix.python-version",
         "__TOX_ENV__": "matrix.tox-env",
-        "__WORKFLOW_TOKEN__": "secrets.WORKFLOW_TOKEN",
     }
 
     lines = []
