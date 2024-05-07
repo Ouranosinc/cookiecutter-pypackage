9f06cab41050c107a49f3e7159e0c64cca88da14
Zeitsperre <10819524+Zeitsperre@users.noreply.github.com>
2024-05-06 20:06:37 -0400

    adjust test

--- {{cookiecutter.project_slug}}/tests/test_{{cookiecutter.project_slug}}.py
+++ {{cookiecutter.project_slug}}/tests/test_{{cookiecutter.project_slug}}.py
@@ -13,7 +13,7 @@ import pytest
 from click.testing import CliRunner
 {%- endif %}
 {% if cookiecutter.command_line_interface|lower == 'click' %}import {{ cookiecutter.project_slug }}.cli as cli{%- endif %}
-from {{ cookiecutter.project_slug }} import {{ cookiecutter.project_slug }}  # noqa: F401
+import {{ cookiecutter.project_slug }}  # noqa: F401
 {%- if cookiecutter.use_pytest == 'y' %}
 
 
