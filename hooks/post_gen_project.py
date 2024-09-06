f020d2d7b0b660ba92ca6d549d625934ef988341
Trevor James Smith <10819524+Zeitsperre@users.noreply.github.com>
2024-09-06 11:11:37 -0400

    update actions versions, use coveralls action, use caching of pip downloads and tox environments, stage support for Python3.13

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -28,6 +28,7 @@ def replace_contents(filepath):
         "__GITHUB_REF__": "github.ref",
         "__GITHUB_REF_NAME__": "github.ref_name",
         "__GITHUB_TOKEN__": "secrets.GITHUB_TOKEN",
+        "__OS__": "matrix.os",
         "__PYTHON_VERSION__": "matrix.python-version",
         "__TOX_ENV__": "matrix.tox-env",
         "__OPENSSF_SCORECARD_TOKEN__": "secrets.OPENSSF_SCORECARD_TOKEN",
